4a92ad42bd1c9720c5ae5f0b5463cf600d0835c9
Mridulbirla13 <mridulbirla13@gmail.com>
2024-05-10 15:18:53 +0530

    update this

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ export default function Navbar(props) {
       <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
       <div className="container-fluid">
         {/* <Link className="navbar-brand" to="/">{props.title}</Link> */}
-        <a className="navbar-brand" href="/">{props.title}</a>
+        <a className="navbar-brand" href="#">{props.title}</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -17,7 +17,7 @@ export default function Navbar(props) {
           <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
             <li className="nav-item">
               {/* <Link className="nav-link active" aria-current="page" to="/">Home</Link> */}
-              <a className="nav-link active" aria-current="page" href="/">Home</a>
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
             </li>
             {/* <li className="nav-item">
               <Link className="nav-link" to="/about">{props.aboutText}</Link>
