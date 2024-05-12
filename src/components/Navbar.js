18ef69b5c55ba4c46f6349eb3d293eeed738df3e
Mridulbirla13 <mridulbirla13@gmail.com>
2024-05-12 17:23:38 +0530

    Update and corrected issue for authentication and navbar dropdown

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar(props) {
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">{props.title}</Link>
         {/* <a className="navbar-brand" href="#">{props.title}</a> */}
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarScroll">
